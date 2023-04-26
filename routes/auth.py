7ec1279e1c43281eb1dbcefa1a4ea98e66e53a6c
violetstar701 <violetstar701@gmail.com>
2023-04-26 23:21:41 +0300

    email verify api

--- routes/auth.py
+++ routes/auth.py
@@ -1,25 +1,86 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends, status, Request
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from schemas.auth import UserRegisterRequest
-from core.database import db
-from utils import get_password_hash
+from typing import Annotated
+from pydantic import EmailStr
+from datetime import datetime
+from random import randbytes
+import hashlib
 
-router = APIRouter(prefix="/api", tags=["auth"])
+from core.database import db
+from utils import get_password_hash, verify_password, create_access_token, user_entity
+from core.email import Email
+
+router = APIRouter(prefix="/api/auth", tags=["auth"])
 
 users_collection = db["users"]
 
 
-@router.post("/register", description="Register User")
-async def register_user(user: UserRegisterRequest):
+@router.post("/register", description="register user")
+async def register_user(user: UserRegisterRequest, request: Request):
     # Check if the user already exists
     existing_user = users_collection.find_one({"email": user.email})
     if existing_user:
         raise HTTPException(
-            status_code=400, detail="Username already registered")
+            status_code=status.HTTP_409_CONFLICT, detail="Account already exist")
 
     # Hash the password and insert the new user into the database
     password_hash = get_password_hash(user.password)
+
     user_dict = user.dict()
     user_dict["password"] = password_hash
-    users_collection.insert_one(user_dict)
+    user_dict["role"] = "user"
+    user_dict["is_verified"] = False
+    user_dict["google_sign"] = False
+    user_dict["created_at"] = datetime.utcnow()
+    user_dict["updated_at"] = user_dict["created_at"]
 
-    return {"message": "User registered successfully"}
+    result = users_collection.insert_one(user_dict)
+    # new_user = users_collection.find_one({"_id": result.inserted_id})
+
+    # try:
+    token = randbytes(10)
+    hashed_code = hashlib.sha256()
+    hashed_code.update(token)
+    verification_code = hashed_code.hexdigest()
+    users_collection.find_one_and_update({"_id": result.inserted_id}, {"$set": {
+        "verification_code": verification_code, "updated_at": datetime.utcnow()}})
+    print(token.hex())
+    # url = f"{request.url.scheme}://{request.client.host}:{request.url.port}/api/auth/verifyemail/{token.hex()}"
+    # await Email(user_entity(new_user), url, [EmailStr(user.email)]).sendVerificationCode()
+    # except Exception as error:
+    #     users_collection.find_one_and_update({"_id": result.inserted_id}, {
+    #                                          "$set": {"verification_code": None, "updated_at": datetime.utcnow()}})
+    #     raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+    #                         detail="There was error sending email")
+    return {"status": "success", "message": "Verification token successfully sent"}
+
+
+@router.get('/verifyemail/{token}')
+async def verify_me(token: str):
+    hashed_code = hashlib.sha256()
+    hashed_code.update(bytes.fromhex(token))
+    verification_code = hashed_code.hexdigest()
+    result = users_collection.find_one_and_update({"verification_code": verification_code}, {"$set": {
+                                                  "verification_code": None, "is_verified": True, "updated_at": datetime.utcnow()}}, new=True)
+    if not result:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="Invalid verification code or account already verified")
+    return {
+        "status": "success",
+        "message": "Account verified successfully"
+    }
+
+
+@router.post("/login", description="login user")
+async def login_user(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user = users_collection.find_one({"email": form_data.username})
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Incorrect username", headers={"WWW-Authenticate": "Bearer"})
+    if not verify_password(form_data.password, user["password"]):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Incorrect password", headers={"WWW-Authenticate": "Bearer"})
+
+    access_token = create_access_token(data={"sub": user["email"]})
+    return {"access_token": access_token}
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
+from datetime import timedelta, datetime
 from passlib.context import CryptContext
+from jose import jwt
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
@@ -13,3 +15,26 @@ def verify_password(plain_password, hashed_password):
 
 def get_password_hash(password):
     return pwd_context.hash(password)
+
+
+def create_access_token(data: dict):
+    expires_delta = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    encode_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encode_jwt
+
+
+def user_entity(user) -> dict:
+    return {
+        "id": str(user["_id"]),
+        "full_name": user["full_name"],
+        "email": user["email"],
+        "role": user["role"],
+        "is_verified": user["is_verified"],
+        "password": user["password"],
+        "phone": user["phone"],
+        "created_at": user["created_at"],
+        "updated_at": user["updated_at"]
+    }
